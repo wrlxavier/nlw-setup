61c4c82d8c21ef22d6626194b8762083270a5c5d
wrlxavier <warleyxavier.fernandes@gmail.com>
2023-01-17 19:07:24 -0300

    Configuração do CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import Fastfy from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 
 const app = Fastfy()
 const prisma = new PrismaClient()
+
+app.register(cors)
+
+//Exemplo de especificação de cors (quais endereços podem acessar a API)
+//Em produção, usa-se este tipo de configuração
+/* app.register(cors, {
+    origin: ['http://localhost:3000']
+}) */
 
 app.get('/', async () => {
 
